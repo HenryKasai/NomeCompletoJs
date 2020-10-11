fc42030047ac2843387ca7c78a22cd6871d08408
HenryKasai <henrykasai1775@gmail.com>
2020-10-11 13:49:46 -0300

    Exercício Resolvido

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let primeironome = prompt("Digite seu primeiro nome.");
 if (primeironome) { 
-        let sobrenome = prompt("Digite seu sobrenome.");
+    let sobrenome = prompt("Digite seu sobrenome.");
     if (sobrenome) {
         alert(`Nome Completo: ${primeironome} ${sobrenome}.`);
         alert(`Nome de catálogo: ${sobrenome.toUpperCase()}, ${primeironome}.`);
